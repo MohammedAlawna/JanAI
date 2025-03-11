8a104d90236244a878273536a22e6bb3ea48dbfc
Mohammed Alawna <88289155+MohammedAlawna@users.noreply.github.com>
2025-03-11 06:17:30 +0000

    Env Varialbes, Open API KEY and Bot. (Load)

--- AIModel.py
+++ AIModel.py
@@ -3,3 +3,14 @@ import openai
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+
+
+#Load Env Variables (Bot, OpenAI API):
+load_dotenv()
+DISCORD_BOT_TOKEN=os.getenv("DISCORD_BOT_TOKEN")
+OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+
+#OpenAI Key Configuration:
+openai.api_key = OPENAI_API_KEY
+
+#Bot Command Specifications:
